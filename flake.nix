269961d13d45ffb6aaf9b97de8d37ddcbf010baa
Emory Merryman <emory.merryman@gmail.com>
2025-10-26 14:41:55 -0400

    secret

---
+++ flake.nix
@@ -0,0 +1,129 @@
+{
+    inputs = { } ;
+    outputs =
+        { self } :
+            {
+                lib =
+                    { age , writeShellApplication } :
+                        let
+                            implementation =
+                                { encrypted , identity } :
+                                    {
+                                        init =
+                                            { resources , self } :
+                                                let
+                                                    application =
+                                                        writeShellApplication
+                                                            {
+                                                                name = "init" ;
+                                                                runtimeInputs = [ age ] ;
+                                                                text =
+                                                                    ''
+                                                                        age --identity ${ identity } ${ encrypted } > /mount/secret
+                                                                        chmod 0400 /mount/secret
+                                                                    '' ;
+                                                            } ;
+                                                    in "${ application }/bin/init" ;
+                                        targets = [ "secret" ] ;
+                                    } ;
+                            in
+                                {
+                                    check =
+                                        {
+                                            expected ,
+                                            encrypted ,
+                                            identity ,
+                                            failure ,
+                                            mkDerivation ,
+                                            resources ? null ,
+                                            self ? null
+                                        } :
+                                            mkDerivation
+                                                {
+                                                    installPhase =
+                                                        ''
+                                                            execute-test-attributes "$out"
+                                                            execute-test-init "$out"
+                                                            execute-test-targets "$out"
+                                                        '' ;
+                                                    name = "check" ;
+                                                    nativeBuildInputs =
+                                                        [
+                                                            (
+                                                                writeShellApplication
+                                                                    {
+                                                                        name = "execute-test-attributes" ;
+                                                                        runtimeInputs = [ coreutils ( failure.implementation "4187683d" ) ] ;
+                                                                        text =
+                                                                            let
+                                                                                x = implementation { encrypted = encrypted ; identity = identity ; } ;
+                                                                                observed = builtins.attrNames x ;
+                                                                                in
+                                                                                    if [ "secret" ] == observed
+                                                                                    then
+                                                                                        ''
+                                                                                            OUT="$1"
+                                                                                            touch "$OUT"
+                                                                                        ''
+                                                                                    else
+                                                                                        ''
+                                                                                            OUT=$1
+                                                                                            touch "$OUT"
+                                                                                            failure attributes
+                                                                                        '' ;
+                                                                    }
+                                                            )
+                                                            (
+                                                                writeShellApplication
+                                                                    {
+                                                                        name = "execute-test-init" ;
+                                                                        runtimeInputs = [ coreutils ( failure.implementation "9507ef9d" ) ] ;
+                                                                        text =
+                                                                            let
+                                                                                x = implementation { encrypted = encrypted ; identity = identity ; } ;
+                                                                                observed = builtins.toString ( x { resources = resources ; self = self ; } ) ;
+                                                                            in
+                                                                                if expected == observed then
+                                                                                    ''
+                                                                                        OUT="$1"
+                                                                                        touch "$OUT"
+                                                                                    ''
+                                                                                else
+                                                                                    ''
+                                                                                        OUT="$1"
+                                                                                        touch "$OUT"
+                                                                                        failure init "We expected ${ expected } but we observed ${ observed }"
+                                                                                    '' ;
+                                                                    }
+                                                            )
+                                                            (
+                                                                writeShellApplication
+                                                                    {
+                                                                        name = "execute-test-targets" ;
+                                                                        runtimeInputs = [ coreutils ( failure.implementation "8eadd518" ) ] ;
+                                                                        text =
+                                                                            let
+                                                                                x = implementation { encrypted = encrypted ; identity = identity ; } ;
+                                                                                observed = x.targets ;
+                                                                                in
+                                                                                    if [ "secret" ] == observed
+                                                                                    then
+                                                                                        ''
+                                                                                            OUT="$1"
+                                                                                            touch "$OUT"
+                                                                                        ''
+                                                                                    else
+                                                                                        ''
+                                                                                            OUT=$1
+                                                                                            touch "$OUT"
+                                                                                            failure targets
+                                                                                        '' ;
+                                                                    }
+                                                            )
+                                                        ] ;
+                                                    src = ./. ;
+                                                } ;
+                                    implementation = implementation ;
+                                } ;
+            } ;
+}
