2f072a3f6e4a180cc9eccec651cc64c9de54917e
Emory Merryman <emory.merryman@gmail.com>
2025-11-04 10:37:18 -0500

    secret

--- flake.nix
+++ flake.nix
@@ -62,8 +62,8 @@
                                                                                     OUT="$1"
                                                                                     touch "$OUT"
                                                                                     ${ if [ "init" "targets" ] != builtins.attrNames instance then ''failure c8b01223 "We expected the secret names to be init targets but we observed ${ builtins.toJSON ( builtins.attrNames instance ) }"'' else "#" }
-                                                                                    ${ if expected != init then ''failure 2e2ca58a "We expected the init to be ${ builtins.toString expected } but we observed ${ builtins.toString init }"'' else "#" }
-                                                                                    ${ if [ "secret" ] != instance.targets then ''failure 2c9823c8 "We expected the targets to be secret but we observed ${ builtins.toJSON instance.targets }"'' else "#" }
+                                                                                    ${ if expected != init then ''failure 2e2ca58a "We expected the secret init to be ${ builtins.toString expected } but we observed ${ builtins.toString init }"'' else "#" }
+                                                                                    ${ if [ "secret" ] != instance.targets then ''failure 2c9823c8 "We expected the secret targets to be secret but we observed ${ builtins.toJSON instance.targets }"'' else "#" }
                                                                                 '' ;
                                                                 }
                                                         )
